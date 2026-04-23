1c242c2aadd6629b0a6443e2c0d0553e98f04fe2
Nicolas Figueroa <dev.figueroanicolas22@gmail.com>
2026-04-23 17:17:51 -0400

    feat: add header and projects section

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import astroNoEmail from 'astro-noemail';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss(), astroNoEmail()]
+
   }
 });
